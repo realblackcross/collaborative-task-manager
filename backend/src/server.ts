0a5ad3250627942390e6bb607cc98e64456a474d
realblackcross <akarshtyagi2004@gmail.com>
2025-12-26 22:39:53 +0530

    Fix CORS for production frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -36,7 +36,7 @@ io.on("connection", (socket) => {
 // app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
